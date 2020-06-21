196f1a51d16d39d696caccd556a2814185a9e461
Salvatore Tosti <saltosti@gmail.com>
2020-06-21 13:05:20 -0400

    Added hypothesis tests and logging to app

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,13 @@
 from app import app
-from flask import render_template
-
+from flask import render_template, jsonify, request
+from loguru import logger
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+@app.route("/dummy", methods=['POST'])
+def dummy():
+    string = request.form.get('string')
+    return jsonify({"dummy":string})
--- test/test_routes.py
+++ test/test_routes.py
@@ -1,5 +1,8 @@
 from app import app
 import pytest
+import json
+from hypothesis import given
+from hypothesis.strategies import text
 
 @pytest.fixture(scope='module')
 def client():
@@ -14,4 +17,15 @@ def test_index(client):
 def test_404(client):
     response = client.get('/404', follow_redirects=True)
     assert 404 == response.status_code
-
+    
+@given(text())
+def test_text_route(client, string):
+    response = client.post(
+        '/dummy', 
+        data = {
+            'string': string
+            },
+        follow_redirects=True)
+    data = json.loads(response.get_data(as_text=True))
+    print(string)
+    assert string == data['dummy']
